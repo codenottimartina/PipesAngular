6599d72e36e9a6c94f868681963895bacc97433c
Martina <martina.codenotti2@gmail.com>
2024-03-18 10:15:55 +0100

    fino a lezione 253

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter',
+  pure: false, // ogni volta che viene aggiunto un server all'array la pipe viene richiamata
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any, filterString: string, propName: string): any {
+    if(value.length == 0 || filterString == ''){
+      return value;
+    }
+
+    const resultArray = [];
+    for(const item of value){
+      if(item[propName] == filterString){
+        resultArray.push(item);
+      }
+    }
+    
+    return resultArray;
+  }
+
+}
